f60a279e9040bf4b39852eeb1809eac0f1797905
Unknown <fanil@demolit.ru>
2024-09-11 17:26:39 +0300

    added_script

--- service_scripts/updateVisibleLang.js
+++ service_scripts/updateVisibleLang.js
@@ -1,5 +1,5 @@
 const db = require(".././database")
 db.connection.authenticate().then(async () => {
-    await db.models.Category.update({ visibleLang: ['en', 'es', 'fr', 'ru'] }, { where: {} })
-    await db.models.Article.update({ visibleLang: ['en', 'es', 'fr', 'ru'] }, { where: {} })
+    await db.models.Category.update({ visible: true }, { where: {} })
+    // await db.models.Article.update({ visibleLang: ['en', 'es', 'fr', 'ru'] }, { where: {} })
 })
